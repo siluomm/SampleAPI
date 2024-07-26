28bcd2e6a16b03f26cbd98cd36e658bbb0d400b6
siluomm <siluomm@gmail.com>
2024-07-26 23:33:25 +0300

    Update index.js and package.json; add certs directory

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 const {
 	Pool
 } = require('pg');
@@ -11,13 +13,21 @@ dotenv.config();
 
 const app = express();
 const port = 3000;
+const caCert  = fs.readFileSync(path.join(__dirname, 'certs/ca.pem')).toString();
+
+
 const pool = new Pool({
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
 	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
+	host: process.env.DB_HOST,
 	database: process.env.DB_NAME,
-});
+	password: process.env.DB_PASSWORD,
+	port: process.env.DB_PORT,
+	ssl: {
+	  ca: caCert, // Include the CA certificate
+	  rejectUnauthorized: true // Ensure the certificate is valid (recommended for production)
+	}
+  });
+
 
 app.use(cors());
 
